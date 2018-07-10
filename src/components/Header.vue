e4012de4d9b83a2a3479120494f77786a40b3e1d
TanBinghua <14718152017@163.com>
2018-07-11 00:01:22 +0800

    views

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,3 +3,9 @@
     <h3>Header</h3>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  line-height: 80px;
+}
+</style>
--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -55,6 +55,10 @@ export default {
 </script>
 
 <style>
+.notes {
+  margin: 64px 214px 0 96px;
+}
+
 .change-bg {
   background: #fff;
 }
